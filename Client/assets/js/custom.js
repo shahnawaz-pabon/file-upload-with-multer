21856631a37436b628a185eea18b3918cf2e8673
pabon <shahnawazhossan531@gmail.com>
2019-09-27 22:52:39 +0600

    Typed js, file input added and styled

---
+++ Client/assets/js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  var typed = new Typed(".typed-text", {
+      strings: ["Upload File with Multer", "by", "Pabon"],
+      typeSpeed: 30,
+      // backspacing speed
+      backSpeed: 20,
+      // time before backspacing
+      backDelay: 500,
+      loop: true,
+      startDelay: 1000,
+      showCursor: false
+  });
+
+});
